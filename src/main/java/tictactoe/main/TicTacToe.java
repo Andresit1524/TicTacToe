ef493341f24f6c630f15578696593c35f9ea6f02
Hayran Andrés López <andreslopez01182009@gmail.com>
2025-10-09 08:01:51 -0500

    Commit inicial

---
+++ src/main/java/tictactoe/main/TicTacToe.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tictactoe.main;
+
+/**
+ *
+ * @author Andrés López
+ */
+public class TicTacToe {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
