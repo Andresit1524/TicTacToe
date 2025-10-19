da60ae97eab91a6faf914808715dc658cedb3371
Hayran Andrés López <andreslopez01182009@gmail.com>
2025-10-18 21:57:16 -0500

    Primer implementación de la clase `Board`

---
+++ src/main/java/tictactoe/business/GameEngine.java
@@ -0,0 +1,9 @@
+package tictactoe.business;
+
+/**
+ * @author Andrés López
+ * @version 1
+ */
+public class GameEngine {
+
+}
---
+++ src/main/java/tictactoe/data/Board.java
@@ -0,0 +1,118 @@
+package tictactoe.data;
+
+/**
+ * Almacena los elementos del tablero del juego como una lista de números
+ * enteros:
+ * 
+ * - 0: vacío
+ * - 1: jugador 1
+ * - -1: jugador 2
+ * 
+ * @author Andrés López
+ * @version 1
+ */
+public class Board {
+    // Arreglo que almacena el tablero
+    private int[][] board;
+    private boolean isFull;
+
+    public Board() {
+        board = new int[3][3];
+        isFull = false;
+    }
+
+    /**
+     * Evalúa si el tablero esta lleno
+     */
+    public boolean isFull() {
+        // Empieza asumiendo que esta lleno
+        isFull = true;
+
+        // Si hay un cero, entonces no está lleno
+        for (int[] row : board) {
+            for (int square : row) {
+                if (square == 0) {
+                    isFull = false;
+
+                    // Retorna de inmediato
+                    return isFull;
+                }
+            }
+        }
+
+        return isFull;
+    }
+
+    /**
+     * Evalúa si el recuadro seleccionado está disponible
+     */
+    public boolean isAvailableSquare(int row, int col) {
+        // Si está lleno, no hay recuadro disponible
+        if (isFull) {
+            return false;
+        }
+
+        return board[row][col] == 0;
+    }
+
+    /**
+     * Establece el recuadro seleccionado del tablero con el valor indicado, si es
+     * movimiento válido. Retorna false si el movimiento no es válido.
+     */
+    public boolean setSquare(int movement, int row, int col) {
+        // Verifica que el recuadro está disponible
+        if (!isAvailableSquare(row, col)) {
+            return false;
+        }
+
+        // Escribe en el recuadro y retorna
+        board[row][col] = movement;
+
+        return true;
+    }
+
+    /**
+     * Convierte el tablero en una representación de string que pueda ser impresa.
+     * También hace conversión del signo a O (jugador 1) o X (jugador 2).
+     */
+    public String getBoard() {
+        String boardString = "";
+
+        // Recorre cada fila
+        for (int[] row : board) {
+            boardString += "| ";
+
+            // Recorre cada elemento de la fila
+            for (int square : row) {
+                switch (square) {
+                    case 0:
+                        boardString += " ";
+                        break;
+                    case 1:
+                        boardString += "O";
+                        break;
+                    case -1:
+                        boardString += "X";
+                        break;
+                    default:
+                        break;
+                }
+
+                // Espacio entre elementos
+                boardString += " | ";
+            }
+
+            // Salto de línea entre filas
+            boardString += "\n";
+        }
+
+        return boardString;
+    }
+
+    /**
+     * Extrae la casilla seleccionada en forma de número
+     */
+    public int getSquare(int row, int col) {
+        return board[row][col];
+    }
+}
